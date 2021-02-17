973e0400debd7fec96a29de0257194c10249322a
Francisco Javier Lledó Fuster <51862214+javilledo@users.noreply.github.com>
2021-02-17 19:36:59 +0100

    Controller, router y modelo

---
+++ controllers/usuarios.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from 'express';
+
+export const getUsuarios = (req: Request, res: Response) => {
+
+    res.json({
+        msg: 'getUsuarios'
+    })
+
+}
+
+export const getUsuario = (req: Request, res: Response) => {
+
+    const {id} = req.params;
+
+    res.json({
+        msg: 'getUsuario',
+        id
+    })
+
+}
+
+export const postUsuario = (req: Request, res: Response) => {
+
+    const {body} = req.params;
+
+    res.json({
+        msg: 'postUsuario',
+        body
+    })
+    
+}
+
+export const putUsuario = (req: Request, res: Response) => {
+
+    const {id} = req.params;
+    const {body} = req.params;
+
+    res.json({
+        msg: 'putUsuario',
+        id,
+        body
+    })
+    
+}
+
+export const deleteUsuario = (req: Request, res: Response) => {
+
+    const {id} = req.params;
+
+    res.json({
+        msg: 'deleteUsuario',
+        id
+    })
+    
+}
--- models/server.ts
+++ models/server.ts
@@ -1,13 +1,25 @@
-import express, {Application} from 'express'
-
+import express, { Application } from 'express'
+import userRoutes from '../routes/usuario';
 export default class Server {
 
     private app: Application;
     private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
 
     constructor(){
         this.app = express();
         this.port = process.env.PORT || '8000';
+
+        //Definir mis rutas
+        this.routes();
+
+    }
+
+    routes(){
+
+        this.app.use(this.apiPaths.usuarios, userRoutes)
 
     }
 
